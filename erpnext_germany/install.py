b35d4a4a0cee1e0cf1412f2b8d6c1a765075fdc5
scdanieli <23150094+scdanieli@users.noreply.github.com>
2024-02-15 16:57:20 +0100

    refactor: generalize add comments function

---
+++ erpnext_germany/install.py
@@ -0,0 +1,61 @@
+from csv import DictReader
+
+import frappe
+from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
+from frappe.custom.doctype.property_setter.property_setter import make_property_setter
+
+
+def after_install():
+	custom_fields = frappe.get_hooks("germany_custom_fields")
+	create_custom_fields(custom_fields)
+	make_property_setters()
+	import_data()
+	insert_custom_records()
+
+
+def import_data():
+	for doctype, filename in (
+		("Religious Denomination", "religious_denomination.csv"),
+		("Employee Health Insurance", "employee_health_insurance.csv"),
+	):
+		if not frappe.db.exists("DocType", doctype):
+			continue
+
+		path = frappe.get_app_path("erpnext_germany", "data", filename)
+		import_csv(doctype, path)
+
+
+def import_csv(doctype, path):
+	with open(path) as csvfile:
+		reader = DictReader(csvfile)
+		for row in reader:
+			if frappe.db.exists(doctype, row):
+				continue
+
+			doc = frappe.new_doc(doctype)
+			doc.update(row)
+			doc.insert()
+
+
+def make_property_setters():
+	germany_property_setters = frappe.get_hooks("germany_property_setters")
+	for doctypes, property_setters in germany_property_setters.items():
+		if isinstance(doctypes, str):
+			doctypes = (doctypes,)
+
+		for doctype in doctypes:
+			for property_setter in property_setters:
+				for_doctype = not property_setter[0]
+				make_property_setter(doctype, *property_setter, for_doctype)
+
+
+def insert_custom_records():
+	for custom_record in frappe.get_hooks("germany_custom_records"):
+		filters = custom_record.copy()
+		# Clean up filters. They need to be a plain dict without nested dicts or lists.
+		for key, value in custom_record.items():
+			if isinstance(value, (list, dict)):
+				del filters[key]
+
+		if not frappe.db.exists(filters):
+			frappe.get_doc(custom_record).insert(ignore_if_duplicate=True)
